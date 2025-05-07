2e70949688693b5f7e92f62e4b346a2c0fb0130a
mallwhcre <marykark.mk@gmail.com>
2025-05-07 17:24:29 +0300

    Added a function reading the file and calculates the avg of the day

---
+++ build/lib/AVLTree/CMakeFiles/AVLTree.dir/avlTree.c.o.d
@@ -0,0 +1,4 @@
+lib/AVLTree/CMakeFiles/AVLTree.dir/avlTree.c.o: \
+ /home/mar/Documents/CEID/DataStructs/Project25_2/lib/AVLTree/avlTree.c \
+ /usr/include/stdc-predef.h \
+ /home/mar/Documents/CEID/DataStructs/Project25_2/lib/AVLTree/avlTree.h
---
+++ build/src/CMakeFiles/DataStructsII.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mar/Documents/CEID/DataStructs/Project25_2/src/DataStructsII.c" "src/CMakeFiles/DataStructsII.dir/DataStructsII.c.o" "gcc" "src/CMakeFiles/DataStructsII.dir/DataStructsII.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mar/Documents/CEID/DataStructs/Project25_2/build/lib/AVLTree/CMakeFiles/AVLTree.dir/DependInfo.cmake"
+  "/home/mar/Documents/CEID/DataStructs/Project25_2/build/lib/FileParsing/CMakeFiles/FileParsing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
