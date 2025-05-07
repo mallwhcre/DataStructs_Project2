36358014c61dc0c57ff946e0620a2b3d145884f5
loukbak <loukasbaklavas.uni@gmail.com>
2025-05-07 16:04:42 +0300

    added cmake

---
+++ lib/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_C_
+#define LIB_C_
+
+int add(int a, int b);
+
+#endif
---
+++ src/DataStructsII.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lib.h"
+
+int main()
+{
+
+    printf("sum is %d", add(5, 6));
+
+    return 0;
+}
