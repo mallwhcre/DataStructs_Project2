3a58817e7771b0d46a2b9e50f955e5201767f9fe
mallwhcre <marykark.mk@gmail.com>
2025-05-15 22:43:11 +0300

    Created files for hashing no big changes made

---
+++ lib/HashTable/hashTable.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "commonTypes.h"
+
+
+
+int hash(Record *rec){
+
+    int hashValue=0;
+
+    int year=rec->date.year;
+    int month=rec->date.month;
+    int day=rec->date.day;
+
+
+
+}
---
+++ lib/HashTable/hashTable.h
@@ -0,0 +1,9 @@
+#ifndef HASH_TABLE_
+#define HASH_TABLE_
+
+#include "commonTypes.h"
+
+int hash(Record *rec);
+
+
+#endif
